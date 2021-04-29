ec3249dbf95b35f255b5a23e3d3b1df7b711b2b9
Akadia Kachaochana <akachaochana@gmail.com>
2021-04-28 20:21:16 -0500

    added json files

---
+++ static/js/appPlots.js
@@ -0,0 +1,39 @@
+var grad_data="../data/ga_ny_grad.json"
+console.log(grad_data)
+
+// var dataPoints2 = [];
+// var chart2 = new CanvasJS.Chart("chartContainer2", {
+// 	animationEnabled: true,
+// 	axisX: {
+// 		interval: 1,
+// 		labelMaxWidth: 1000,
+// 		labelFontSize:11,
+// 		reversed: true
+// 	},
+// 	axisY2:{
+// 		interlacedColor: "rgba(1,77,101,.2)",
+// 		gridColor: "rgba(1,77,101,.1)",
+// 		title: "Grad rates by county"
+// 	},
+
+// 	data: [{
+// 		type: 'bar',
+// 		color: "#53B17B",
+// 		//xValueFormatString:"D MM h:mm",
+// 		name: "series1",
+// 		dataPoints: dataPoints2 // this should contain only specific serial number data
+
+// 	}]
+// });
+
+// $( "#second" ).change(function() {
+// 	chart2.options.data[0].dataPoints = [];
+// 	var e = grad_data.getElementById("second");
+// 	var selected = e.options[e.selectedIndex].value;
+// 	console.log(selected)
+// 	dps = grad_data[selected][0];
+// 	for(var i in dps) {
+// 		chart2.options.data[0].dataPoints.push({label: dps[i].label, y: dps[i].y});
+// 	}
+// 	chart2.render();
+// })
